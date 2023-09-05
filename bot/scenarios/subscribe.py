6af176be55e12f6fa4ed39d92de0a466018dad02
Dmitry Kolobaev <kol-di@yandex.ru>
2023-09-05 15:00:05 +0300

    option to (un)subscribe from all at once

--- bot/scenarios/subscribe.py
+++ bot/scenarios/subscribe.py
@@ -10,22 +10,66 @@ from bot.access import \
     REC_ODNOPOZ_ODNOLOT_LS_NO_CONTRACT_223
 
 
+class InvalidInlineQueryParamWarning(Warning):
+    pass
+
+
 def user_callback(user_id):
     return events.CallbackQuery(chats=user_id)
 
 
-@bot.on(events.CallbackQuery(data = re.compile(_RECIPIENT_RIGHT_PREFIX)))
-async def choose_subscriptio_event_handler(event):
+@bot.on(events.CallbackQuery(pattern='all'))
+async def all_subscriptions(event):
     sender = await event.get_sender()
+
     user = User(sender.username)
-    access_right = await event.get_data.decode()  # data comes in form of bytes and should be converted
 
     if user in user_access.get_authorized_users():
+        async with bot.conversation(sender, timeout=60, exclusive=False, replies_are_responses=True) as conv:
+            ask_msg = await conv.send_message(
+                message="Что вы хотите сделать?", 
+                buttons=[
+                    Button.inline('Отписаться от всего', data='u'), Button.inline('Подписаться на все', data='s')
+                ])
+            
+            all_callback = await conv.wait_event(user_callback(sender))
+            match all_callback.data.decode():
+                case 'u':
+                    for right_alias in user_access._alias_to_right.keys():
+                        user_access.remove_acces(user, right_alias)
+                        await all_callback.delete()
+                        await conv.send_message("Вы отписались от всех рассылок")
+                        conv.cancel()
+                case 's':
+                    for right_alias in user_access._alias_to_right.keys():
+                        user_access.grant_acces(user, right_alias)
+                        await all_callback.delete()
+                        await conv.send_message("Вы подписалсиь на все рассылки")
+                        conv.cancel()
+                case _ as var:
+                    warnings.warn(f"Invalid parameter {var}", InvalidInlineQueryParamWarning)
+                    # await all_callback.delete()
+                    await bot.delete_messages(entity=sender, message_ids=ask_msg.id)
+                    conv.cancel()
+
+
+
+@bot.on(events.CallbackQuery(data = re.compile(_RECIPIENT_RIGHT_PREFIX)))
+async def choose_subscription_event_handler(event):
+    print('I entered anyway!')
+    sender = await event.get_sender()
+
+    user = User(sender.username)
+    access_right = event.data.decode()  # data comes in form of bytes and should be converted
+
+    if user in user_access.get_authorized_users():
+        print('found authorized user')
 
         if user_access.is_granted(user, access_right):
-            async with bot.conversation(sender, timeout=60, replies_are_responses=True) as conv:
+            async with bot.conversation(sender, timeout=60, exclusive=False, replies_are_responses=True) as conv:
+                print('user already in mailing list')
 
-                await conv.send_message(
+                ask_msg = await conv.send_message(
                     message="Вы уже подписаны на эту рассылку. Желаете отменить?", 
                     buttons=[
                         Button.inline('Да', data='y'), Button.inline('Нет', data='n')
@@ -41,8 +85,11 @@ async def choose_subscriptio_event_handler(event):
                     case 'n':
                         await del_callback.delete()
                         conv.cancel()
-                    case _:
-                        warnings.warn("Invalid parameter passed through inline query")
+                    case _ as var:
+                        warnings.warn(f"Invalid parameter {var}", InvalidInlineQueryParamWarning)
+                        # await del_callback.delete()
+                        await bot.delete_messages(entity=sender, message_ids=ask_msg.id)
+                        conv.cancel()
 
         else:
             user_access.grant_acces(user, access_right)
